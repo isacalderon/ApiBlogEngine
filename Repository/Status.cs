23135905dce1274ecacc62aee83425052479d5e5
isacalderon <139363059+isatribe@users.noreply.github.com>
2023-09-01 18:06:07 -0500

    add sql scripts and controllers

---
+++ Repository/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiBlogEngine.Repository;
+
+public partial class Status
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<PostStatus> PostStatuses { get; set; } = new List<PostStatus>();
+}
