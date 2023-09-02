bf6e14653839fb4438e7805ad195f21488490ef6
isacalderon <139363059+isatribe@users.noreply.github.com>
2023-09-02 17:21:56 -0500

    search the published posts

--- Services/IPostService.cs
+++ Services/IPostService.cs
@@ -3,9 +3,6 @@ public interface IPostService
 {
     IEnumerable<PostDto> GetAllPostsAsyn();
     IEnumerable<PostDto> GetPostsByAuthor(string email);
-    IEnumerable<PostDto> GetPostByStatus(Enum status);
-    PostDto GetPostsById(int id);
     PostDto CreatePostAsync(string email, PostDto newPost);
     Boolean UpdatePostAsync(PostDto postToBeUpdated, string email);
-    Task DeletePostAsync(int postId);
 }
--- Services/PostService.cs
+++ Services/PostService.cs
@@ -7,6 +7,8 @@ public class PostServices : IPostService
 
     private readonly BlogEngineContext _context;
 
+    private readonly IPostStatusService _postStatusService;
+
     public enum Status
     {
         Draft = 1,
@@ -30,23 +32,24 @@ public class PostServices : IPostService
 
     }
 
-    public PostServices(ILogger<PostsController> logger, BlogEngineContext context)
+    public PostServices(ILogger<PostsController> logger, BlogEngineContext context, IPostStatusService postStatusService)
     {
         _logger = logger;
         _context = context;
+        _postStatusService = postStatusService;
     }
     public PostDto CreatePostAsync(String email, PostDto newPost)
     {
         _logger.LogInformation("CreatePost");
         // validate the user Rol 
-        User user = _context.Users.Where(u => u.Email == email).FirstOrDefault();
+        User user = _context.Users.Where(u => u.Email == email).FirstOrDefault()?? throw new Exception("User not found");
         if( user != null && user.Role != (int)Role.Writer){
             throw new Exception("User is not a writer");
         }
         // create the post
         Post post = new Post();
-        post.Title = newPost.Title;
-        post.Content = newPost.Content;
+        post.Title = newPost.Title?? throw new Exception("Title is required");
+        post.Content = newPost.Content?? throw new Exception("Content is required");
         post.Author = user.Id;
         post.CreatedAt = DateTime.Now;
         post.UpdatedAt = DateTime.Now;
@@ -57,31 +60,21 @@ public class PostServices : IPostService
 
     }
 
-    public Task DeletePostAsync(int postId)
-    {
-        throw new NotImplementedException();
-    }
-
     public IEnumerable<PostDto> GetAllPostsAsyn()
     {
-       IEnumerable<Post> listPost = _context.Posts.ToList(); 
-       List<PostDto> listPostDto = new List<PostDto>();
+        // Search all the Published post
+       IEnumerable<int> publishedPost = _postStatusService.GetPostsByStatus((int)Status.Published);
+       List<Post> listPost = _context.Posts.Where(p => publishedPost.Contains(p.Id)).ToList();
        _logger.LogInformation($"GetPosts: {listPost.Count()}");
-       
-       foreach(Post post in listPost){
-           PostDto postDto = new PostDto();
-           postDto.Title = post.Title;
-           postDto.Content = post.Content;
-           listPostDto.Add(postDto);
-       }
+       List<PostDto> listPostDto = new List<PostDto>();
+       listPost.ForEach(p => listPostDto.Add(new PostDto(){
+           Id = p.Id,
+           Title = p.Title,
+           Content = p.Content}));
+     
        _logger.LogInformation($"Final get Post: {listPostDto.Count()}");
        
        return listPostDto;
-    }
-
-    public IEnumerable<PostDto> GetPostByStatus(Enum status)
-    {
-        throw new NotImplementedException();
     }
 
     public IEnumerable<PostDto> GetPostsByAuthor(string email)
@@ -97,32 +90,23 @@ public class PostServices : IPostService
 
     }
 
-    public PostDto GetPostsById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
     public Boolean UpdatePostAsync(PostDto postToBeUpdated, string email)
     {
         _logger.LogInformation("UpdatePost");
         // validate the user Rol 
-        User user = _context.Users.Where(u => u.Email == email).FirstOrDefault();
+        User user = _context.Users.Where(u => u.Email == email).FirstOrDefault()?? throw new Exception("User not found");
         if( user != null && user.Role != (int)Role.Writer){
             throw new Exception("User is not a writer");
         }
         // create the post
-        Post post = _context.Posts.Where(p => p.Id == postToBeUpdated.Id).FirstOrDefault();
-        if(post == null){
-            throw new Exception("Post not found");
-        }
-        if(post.Author != user.Id){
+        Post post = _context.Posts.Where(p => p.Id == postToBeUpdated.Id).FirstOrDefault()?? throw new Exception("Post not found");
+       
+        if(post.Author != user?.Id){
             throw new Exception("User is not the author of the post");
         }
-        // if(post.Status == (int)Status.Published || post.Status == (int)Status.Submitted){
-        //     throw new Exception("Post is not avaible to edit");
-        // }
-        post.Title = postToBeUpdated.Title;
-        post.Content = postToBeUpdated.Content;
+
+        post.Title = postToBeUpdated.Title?? post.Title;
+        post.Content = postToBeUpdated.Content?? post.Content;
         post.UpdatedAt = DateTime.Now;
         _context.Posts.Update(post);
         _context.SaveChanges();
