7f3f787ebd0e0368410f4ccec38c461b9a04cd0f
isacalderon <139363059+isatribe@users.noreply.github.com>
2023-09-02 00:34:56 -0500

    Agregar el comentario

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -4,9 +4,29 @@ using System.Security.Claims;
 
 [ApiController]
 [Route("api/")]
-public class CommentController:  ControllerBase{
+public class CommentController:ControllerBase {
 
-private readonly ILogger<PostsController> _logger;
+    private readonly ILogger<PostsController> _logger;
+    private readonly ICommentService _commentService;
+
+    public CommentController(ILogger<PostsController> logger, ICommentService commentService)
+    {
+        _logger = logger;
+        _commentService = commentService;
+    }
+
+    [HttpPost("posts/comments")]
+    [Authorize]
+    public ActionResult<CommentDto> CreateComment(CommentDto comment)
+    {
+        string? authorEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+        if (authorEmail == null)
+        {
+            return Unauthorized();
+        }
+        return _commentService.CreateCommentAsync(authorEmail, comment);
+    }
+
 
 
 }
--- Dto/CommentDto.cs
+++ Dto/CommentDto.cs
@@ -5,7 +5,5 @@ public class CommentDto
     [Required]
     public string? Comment { get; set; }
 
-    [Required]
-    public string? Author { get; set; }
     public int PostId { get; set; }
 }
